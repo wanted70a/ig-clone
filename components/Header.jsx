bb6e26deffe6edab16b3c7c34f3bd7ea97a36def
stefanmilic <stefanmilic189@gmail.com>
2022-06-25 13:47:00 +0200

    add menu and profile picture

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,40 +1,51 @@
 import React from "react";
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/outline";
+import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
-      {/* LEFT */}
-      <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png"
-          layout="fill"
-          className="object-contain"
-          alt="iglogo"
-        />
-      </div>
-      <div className="h-24 w-10 relative lg:hidden cursor-pointer">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/480px-Instagram-Icon.png"
-          layout="fill"
-          className="object-contain"
-          alt="iglogo"
-        />
-      </div>
-      {/* MIDDLE */}
-      <div className="relative mt-2">
-        <div className="absolute top-2 left-2">
-          <SearchIcon class="h-5 text-gray-500" />
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/* LEFT */}
+        <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png"
+            layout="fill"
+            className="object-contain"
+            alt="iglogo"
+          />
         </div>
-        <input
-          type="text"
-          placeholder="search"
-          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-        />
+        <div className="h-24 w-10 relative lg:hidden cursor-pointer">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/480px-Instagram-Icon.png"
+            layout="fill"
+            className="object-contain"
+            alt="iglogo"
+          />
+        </div>
+        {/* MIDDLE */}
+        <div className="relative mt-2">
+          <div className="absolute top-2 left-2">
+            <SearchIcon class="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+        {/* RIGHT */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img
+            src="https://thumbs.dreamstime.com/b/vector-illustration-avatar-dummy-logo-collection-image-icon-stock-isolated-object-set-symbol-web-137160339.jpg"
+            alt="user image"
+            className="h-10 rounded-full"
+          />
+        </div>
       </div>
-      {/* RIGHT */}
-      <h1>RIGTH</h1>
     </div>
   );
 }
