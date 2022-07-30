265041155adb8872b954e9c03f66280b825e1a87
stefanmilic <stefanmilic189@gmail.com>
2022-07-30 21:15:19 +0200

    Make Modal with react-modal

--- components/UploadModa.jsx
+++ components/UploadModa.jsx
@@ -1,13 +1,21 @@
+import Modal from "react-modal";
 import { useRecoilState } from "recoil";
 import { modalState } from "../atom/modalAtom";
 
 const UploadModal = () => {
   const [showModal, setShowModal] = useRecoilState(modalState);
   return (
-    <div>
-      <p>MODAL</p>
-      {showModal && <p>Modal is Open</p>}
-    </div>
+    <Modal
+      isOpen={showModal}
+      className="max-w-lg w-[90%] h-[300px] absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+      onRequestClose={() => setShowModal(false)}
+      ariaHideApp={false}
+    >
+      <div className="flex flex-col justify-center items-center h-[100%]">
+        <p>MODAL</p>
+        {showModal && <p>Modal is Open</p>}
+      </div>
+    </Modal>
   );
 };
 
